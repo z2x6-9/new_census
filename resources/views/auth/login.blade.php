031081885087048e839bd851833a3fec9d7284a2
Yousif <yossifmazen@gmail.com>
2023-12-07 14:45:09 +0300

    login design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,52 +2,127 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
     <main class="form-signin w-100 m-auto">
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <h1 class="h3 my-4 fw-normal">الرجاء تسجيل الدخول</h1>
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="username" :value="__('أسم ألمستخدم')" />
-            <x-text-input id="username" class="form-control" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
-        </div>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <h1 class="h3 my-4 fw-normal">الرجاء تسجيل الدخول</h1>
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="username" :value="__('أسم ألمستخدم')" />
+                <x-text-input id="username" class="form-control" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+                <x-input-error :messages="$errors->get('username')" class="mt-2" />
+            </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('كلمة المرور')" />
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('كلمة المرور')" />
 
-            <x-text-input id="password" class="form-control"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                <x-text-input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('تذكرني') }}</span>
-            </label>
-        </div>
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                    <span class="ms-2 text-sm text-gray-600">{{ __('تذكرني') }}</span>
+                </label>
+            </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
+                @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('تسجيل الدخول') }}
-            </x-primary-button>
-        </div>
-    </form>
-</main>
-<footer class="bg-dark text-light text-center p-3 fixed-bottom mt-5">
-    &copy; 2023 تم تطوير هذا الموقع بواسطة <a class="text-info" target="_blank"
-        href="https://www.instagram.com/soft_4_you_dev/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==">Soft
-        4 You</a>, كل الحقوق محفوظة.
-</footer>
+                <x-primary-button class="ms-3">
+                    {{ __('تسجيل الدخول') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </main>
+    
 </x-guest-layout>
+
+
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>موقع التعداد</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
+
+    <style>
+        html,
+        body {
+            height: 100%;
+        }
+
+        body {
+            display: flex;
+            align-items: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #f5f5f5;
+        }
+
+        .form-signin {
+            background-color: #fff;
+            max-width: 330px;
+            padding: 15px;
+        }
+
+        .form-signin .form-floating:focus-within {
+            z-index: 2;
+        }
+
+        .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
+        .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+    </style>
+</head>
+
+<body class="text-center">
+    <main class="form-signin w-100 m-auto">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <h1 class="h3 my-4 fw-normal">الرجاء تسجيل الدخول</h1>
+
+            <div class="form-floating">
+                <input type="text" name="username" class="form-control" id="floatingInput" placeholder="أسم المستخدم">
+                <label for="floatingInput">أسم المستخدم</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">كلمة المرور</label>
+            </div>
+
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" name="remember" value="remember-me"> تذكرني
+                </label>
+            </div>
+            <input type="submit" class="w-100 btn btn-lg btn-primary" value="تسجيل الدخول">
+        </form>
+    </main>
+
+    <footer class="bg-dark text-light text-center p-3 fixed-bottom mt-5">
+        &copy; 2023 تم تطوير هذا الموقع بواسطة <a class="text-info" target="_blank" href="https://www.instagram.com/soft_4_you_dev/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==">Soft
+            4 You</a>, كل الحقوق محفوظة.
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
