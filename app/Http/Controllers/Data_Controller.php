f9dff91871ba629f6d248d03a0c92d1f4408658d
z2x6-9 <blackpool6669@gmail.com>
2023-12-08 20:54:42 +0300

    hj

--- app/Http/Controllers/Data_Controller.php
+++ app/Http/Controllers/Data_Controller.php
@@ -21,17 +21,22 @@ class Data_Controller extends Controller
 
                 $male_leaders = FamilyLeader::all()->where('Gender', 'ذكر')->count();
                 $male_members = FamilyMembers::all()->where('Gender', 'ذكر')->count();
+                $male = isset($male_leaders) ? $male_leaders : 0;
+                $male = isset($male_members) ? $male_members : 0;
                 $male = $male_leaders + $male_members;
 
                 $female_leaders = FamilyLeader::all()->where('Gender', 'انثى')->count();
                 $female_members = FamilyMembers::all()->where('Gender', 'انثى')->count();
+                $female = isset($female_leaders) ? $female_leaders : 0;
+                $female = isset($female_members) ? $female_members : 0;
                 $female = $female_leaders + $female_members;
 
-                $total = $male + $female;
+                $total = isset($male) && isset($female) ? $male + $female : 0;
 
                 $data = FamilyLeader::all();
 
                 // تحقق مما إذا كانت البيانات مكررة
+                $SimilarityNumber = isset($SimilarityNumber) ? $SimilarityNumber : 0;
                 foreach ($data as $item) {
                     // احصل على جميع البيانات من قاعدة البيانات التي لها نفس الاسم
                     $duplicates = DB::table('family_leaders')->where('Leader', $item->Leader)->get();
@@ -53,31 +58,11 @@ class Data_Controller extends Controller
                     'female' => $female,
                     'SimilarityNumber' => $SimilarityNumber,
                     'data' => $data,
-                    ]);
+                ]);
             } else {
                 return view('family_head');
             }
         }
-    }
-
-
-    // $users = FamilyLeader::all();
-
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
     }
 
     /**
@@ -96,23 +81,6 @@ class Data_Controller extends Controller
             }
         }
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
     /**
      * Remove the specified resource from storage.
      */
